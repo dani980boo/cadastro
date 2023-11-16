1f8f80455f15fe6c5fa8437a9cc1dfe4f2c121c7
dani980boo <daniel980boo@gmail.com>
2023-11-16 09:33:31 -0300

    bancodedadosuiui

--- app.js
+++ app.js
@@ -16,24 +16,19 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/:nome', (req, res) => {
-  const userNome = req.params.nome;
-  // faça algo com o userId
-  console.log(`O nome do usuário é ${userNome}`);
-});
-
 app.get("/cadastro", (req, res) => {
   res.sendFile(__dirname + "/cadastro.html");
 });
 
+
 app.post("/cadastro", (req, res) => {
-  const { nome, endereco } = req.body;
-  if (!nome || !endereco) {
-    res.status(400).send("Nome e endereço são campos obrigatórios.");
+  const { nome, endereco, email, gender, data_nasc, phone } = req.body;
+  if (!nome || !endereco || !email || !gender || !data_nasc || !phone) {
+    res.status(400).send("Todos os campos são obrigatórios.");
     return;
   }
 
-  const cliente = { nome, endereco };
+  const cliente = { nome, endereco, email, gender, data_nasc, phone };
   connection.query("INSERT INTO clientes SET ?", cliente, (err, result) => {
     if (err) throw err;
     console.log(`Cliente ${nome} cadastrado com sucesso!`);
